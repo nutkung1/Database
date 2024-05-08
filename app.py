b094a359ef00bca2c4485ba81de91f13dcc5867b
nutkung1 <nutrock123123@gmail.com>
2024-05-08 23:58:21 +0700

    version13

--- app.py
+++ app.py
@@ -250,87 +250,11 @@ if st.session_state["authentication_status"]:
             },
         }
 
-        page = st_navbar(["Home", "Course", "CRUD", "Community", "About"], styles=styles)
+        page = st_navbar(["Home", "CRUD", "Community", "About"], styles=styles)
         if page == "Home":
             st.title("Home")
             st.write("Welcome to the Home Page")
             st.image("image/kmutt-websitelogo-01-scaled.jpg", width=600)  
-        elif page == "Course":
-            # Sample course data
-            course_data = {
-                'Course ID': ['CSE101', 'MAT202', 'BIO303', 'CHE404', 'PHY505', 'ECO606', 'PSY707', 'ENG808', 'HIS909'],
-                'Course Name': ['Introduction to Computer Science', 'Linear Algebra', 'Biology Fundamentals',
-                                'Chemistry Essentials', 'Physics Principles', 'Economics for Beginners',
-                                'Psychology Basics', 'Advanced English Literature', 'History of Civilization'],
-                'Instructor': ['Prof. Smith', 'Dr. Johnson', 'Prof. Lee', 'Dr. Brown', 'Prof. Williams',
-                            'Dr. Taylor', 'Prof. Martinez', 'Dr. Anderson', 'Prof. Thompson'],
-                'Instructor_ID': [101, 102, 103, 104, 105, 106, 107, 108, 109],  # Example Instructor IDs
-                'Description': ['An introductory course covering the basics of computer science.',
-                                'A course covering fundamental concepts in linear algebra.',
-                                'An overview of basic concepts in biology and biochemistry.',
-                                'Essential principles of chemistry for beginners for junior.',
-                                'Introduction to fundamental principles of physics.',
-                                'An introductory course to the basics of economics.',
-                                'Understanding the fundamentals of psychology.',
-                                'Exploration of advanced English literary works.',
-                                'A journey through the history of human civilization.'],
-                'Image URL': ['https://images.unsplash.com/photo-1610563166150-b34df4f3bcd6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29tcHV0ZXIlMjBzY2llbmNlfGVufDB8fDB8fHww',
-                            'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Linear_subspaces_with_shading.svg/1200px-Linear_subspaces_with_shading.svg.png',
-                            'https://img.freepik.com/free-vector/science-lab-objects_23-2148488312.jpg?size=338&ext=jpg&ga=GA1.1.1224184972.1715126400&semt=sph',
-                            'https://cdn.britannica.com/86/193986-050-7B2DBB6A/ball-and-stick-model-structure-atoms.jpg',
-                            'https://media.istockphoto.com/id/936903524/vector/blackboard-inscribed-with-scientific-formulas-and-calculations-in-physics-and-mathematics-can.jpg?s=612x612&w=0&k=20&c=sRLsJbHUVOYvZ_M16hti4fF9NM0RysfjAPUQrCJ8o4U=',
-                            'https://www.ntu.edu.sg/images/librariesprovider124/default-album/general-images/sss-webrevamp/sss_econs_coinsb6efd5c0-2a2a-4c0e-aa83-669f44598aea.jpg?Status=Master&sfvrsn=ee6510c_3',
-                            'https://www.udc.edu/social-udc/wp-content/uploads/sites/24/2018/03/Importance-of-Psychology_UDC.jpg',
-                            'https://heritageinstitute.in/wp-content/uploads/2019/01/Advanced-English.png',
-                            'https://dualcreditathome.com/wp-content/uploads/2014/02/history.jpg'],
-                'Course_schoolyear': [2023, 2023, 2024, 2024, 2024, 2023, 2023, 2024, 2023],  # Example school years
-                'Timetable': ['10.30-12.30', '13.00-15.00', '09.00-11.00', '10.30-12.30',
-                            '13.00-15.00', '09.00-11.00', '10.30-12.30', '13.00-15.00', '09.00-11.00']  # Example Timetable
-            }
-            # Create a DataFrame from the course data
-            df_courses = pd.DataFrame(course_data)
-            # Define header background image and text
-            header_bg_image = 'https://cdn.discordapp.com/attachments/1081112018360221696/1237724333326204928/asasdqweq.JPG?ex=663cb01c&is=663b5e9c&hm=950f617aa089f3d67c5e26c60c3e92c6dccd389fe9c86bbfefbd907a2dd3d4cf&'
-            header_text = """ 
-            <h1 style="color: white; text-align: center; margin-left: 40px; margin-top: 40px;">Helping You Grow</h1>
-            <br>
-            <h5 style="color: white; font-weight: 100; text-align: center; margin-left: 40px; margin-top: 10px;">Learning is the compass that guides us through the uncharted territories of knowledge</h5>
-            <h5 style="color: white; font-weight: 100; text-align: center; margin-left: 40px; margin-top: 10px;">illuminating our path with the brilliance of understanding.</h5>
-            """
-
-
-            # Page title
-            # Display header with background image and text
-            st.markdown(
-                f"""
-                <div style="padding: 0; background-size: cover;">
-                    <div style="background-image: url('{header_bg_image}'); filter: blur(2px); position: absolute; top: 0; left: 0; width: 100%; height: 350px; background-size: contain;"></div>
-                    <div style="position: relative; z-index: 0; margin-top: 50px; margin-bottom: 150px;">{header_text}</div>
-                </div>""",unsafe_allow_html=True
-            )
-
-            with st.container(border=True,height=1000):
-                st.markdown(f"""<h2 style=" text-align: center; margin-bottom:30px;">Active courses now</h2>""", unsafe_allow_html=True)
-                col1, col2, col3= st.columns(3)
-                
-                for index, course in df_courses.iterrows():
-                    with eval(f"col{index % 3+ 1}"):  # Alternate between the four columns
-                        col4, col5, col6= st.columns(3)
-                        with col5:
-                            st.markdown(
-                                f"""<div style="height: 133px; width: 200px; overflow: hidden; border-radius: 15px; margin-top:20px;">
-                                    <img src="{course['Image URL']}" alt="{course['Course Name']}" style="width: 100%; height: 100%; object-fit: cover;">
-                                </div>""",
-                                unsafe_allow_html=True
-                            )
-                        st.markdown(f"""<h4 style=" text-align: center; margin-bottom:10px;">{course['Course Name']}</h4>""", unsafe_allow_html=True)
-                        with st.container(border=True,):
-                            st.write(f"**Course ID:** {course['Course ID']}")
-                            st.write(f"**Instructor:** {course['Instructor']} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; **Instructor ID:** {course['Instructor_ID']}")
-                            st.write(f"**Description:** {course['Description']}")
-                            st.write(f"**Course School Year:** {course['Course_schoolyear']}")
-                            st.write(f"**Timetable:** {course['Timetable']}")
-                            st.button(f"**Enrollment** {course['Course ID']}")
         elif page == "CRUD":
             mycursor.execute("SELECT * FROM student")
             data = mycursor.fetchall()
@@ -415,7 +339,154 @@ if st.session_state["authentication_status"]:
 
     #ROLE Student
     elif st.session_state["authentication_status"] and st.session_state["role"] == "student":
-        st.write("Welcome to Student Page")
+        styles = {
+            "nav": {
+                "background-color": "rgb(135, 206, 235)",  # Sky Blue
+            },
+            "div": {
+                "max-width": "32rem",
+            },
+            "span": {
+                "border-radius": "1rem",
+                "color": "rgb(49, 51, 63)",
+                "margin": "0 0.125rem",
+                "padding": "0.4375rem 0.625rem",
+            },
+            "active": {
+                "background-color": "rgba(255, 255, 255, 0.25)",
+            },
+            "hover": {
+                "background-color": "rgba(255, 255, 255, 0.35)",
+            },
+        }
+        student_id = st.session_state["student_id"]
+        page = st_navbar(["Home", "Detail", "Course"], styles=styles)
+        if page == "Home":
+            st.title("Home")
+            st.write("Welcome to the Home Page")
+            st.image("image/kmutt-websitelogo-01-scaled.jpg", width=600) 
+        elif page == "Detail":
+            st.title("Student Detail")
+            mycursor.execute("SELECT * FROM student WHERE student_id = %s", (student_id, ))
+            result = mycursor.fetchall()
+            # Creating a DataFrame
+            invoice_df = pd.DataFrame(result, columns=['student_id', 'student_firstname', 'student_lastname', 'student_gender', 'department_name', 'student_year', 'student_semester', 'student_address', 'student_email', 'student_phone', 'student_dateofbirth'])
+            invoice_df['student_dateofbirth'] = invoice_df['student_dateofbirth'].astype(str)
+            invoice_df['student_role'] = "Student"
+            col1 = st.columns([0.5, 1.8, 5, 5, 1, 1])
+            with col1[1]:
+                st.image("image/360_F_553796090_XHrE6R9jwmBJUMo9HKl41hyHJ5gqt9oz.jpg", width=200)
+            with col1[2]:
+                st.title(f"{result[0][1]} {result[0][2]}")
+                st.markdown(f"<p style='font-size: 20px; padding-bottom: 120px'><strong>Year: {result[0][5]}</strong></p>", unsafe_allow_html=True)
+
+            col = st.columns([0.1, 0.2, 0.2, 0.2, 0.6])
+            with col[1]:
+                st.markdown(f"<p style='font-size: 25px;'><strong>First Name:</strong></p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size: 20px; padding-bottom: 30px'><strong>{str(result[0][1])}</strong></p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size: 25px;'><strong>Email:</strong></p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size: 20px; padding-bottom: 30px'><strong>{str(result[0][8])}</strong></p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size: 25px;'><strong>Department:</strong></p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size: 20px; padding-bottom: 30px'><strong>{str(result[0][4])}</strong></p>", unsafe_allow_html=True)
+                # st.write(str(result[0][1]))
+            with col[2]:
+                st.markdown(f"<p style='font-size: 25px;'><strong>Last name:</strong></p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size: 20px; padding-bottom: 30px'><strong>{str(result[0][2])}</strong></p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size: 25px;'><strong>Gender:</strong></p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size: 20px; padding-bottom: 30px'><strong>{str(result[0][3])}</strong></p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size: 25px;'><strong>Semester:</strong></p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size: 20px; padding-bottom: 30px'><strong>{str(result[0][6])}</strong></p>", unsafe_allow_html=True)
+            with col[3]:
+                st.markdown(f"<p style='font-size: 25px;'><strong>ID:</strong></p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size: 20px; padding-bottom: 30px'><strong>{str(result[0][0])}</strong></p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size: 25px;'><strong>Birthday:</strong></p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size: 20px; padding-bottom: 30px'><strong>{str(result[0][10])}</strong></p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size: 25px;'><strong>Phonenumber:</strong></p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size: 20px; padding-bottom: 30px'><strong>{str(result[0][9])}</strong></p>", unsafe_allow_html=True)
+            with col[4]:
+                st.subheader("Course Detail")
+                st.dataframe(invoice_df)
+            col = st.columns([0.065, 0.4, 0.4])
+            with col[1]:
+                st.markdown(f"<p style='font-size: 25px;'><strong>Address:</strong></p>", unsafe_allow_html=True)
+                st.markdown(f"<p style='font-size: 20px; padding-bottom: 30px'><strong>{str(result[0][7])}</strong></p>", unsafe_allow_html=True)
+        elif page == "Course":
+            # Sample course data
+            course_data = {
+                'Course ID': ['CSE101', 'MAT202', 'BIO303', 'CHE404', 'PHY505', 'ECO606', 'PSY707', 'ENG808', 'HIS909'],
+                'Course Name': ['Introduction to Computer Science', 'Linear Algebra', 'Biology Fundamentals',
+                                'Chemistry Essentials', 'Physics Principles', 'Economics for Beginners',
+                                'Psychology Basics', 'Advanced English Literature', 'History of Civilization'],
+                'Instructor': ['Prof. Smith', 'Dr. Johnson', 'Prof. Lee', 'Dr. Brown', 'Prof. Williams',
+                            'Dr. Taylor', 'Prof. Martinez', 'Dr. Anderson', 'Prof. Thompson'],
+                'Instructor_ID': [101, 102, 103, 104, 105, 106, 107, 108, 109],  # Example Instructor IDs
+                'Description': ['An introductory course covering the basics of computer science.',
+                                'A course covering fundamental concepts in linear algebra.',
+                                'An overview of basic concepts in biology and biochemistry.',
+                                'Essential principles of chemistry for beginners for junior.',
+                                'Introduction to fundamental principles of physics.',
+                                'An introductory course to the basics of economics.',
+                                'Understanding the fundamentals of psychology.',
+                                'Exploration of advanced English literary works.',
+                                'A journey through the history of human civilization.'],
+                'Image URL': ['https://images.unsplash.com/photo-1610563166150-b34df4f3bcd6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29tcHV0ZXIlMjBzY2llbmNlfGVufDB8fDB8fHww',
+                            'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Linear_subspaces_with_shading.svg/1200px-Linear_subspaces_with_shading.svg.png',
+                            'https://img.freepik.com/free-vector/science-lab-objects_23-2148488312.jpg?size=338&ext=jpg&ga=GA1.1.1224184972.1715126400&semt=sph',
+                            'https://cdn.britannica.com/86/193986-050-7B2DBB6A/ball-and-stick-model-structure-atoms.jpg',
+                            'https://media.istockphoto.com/id/936903524/vector/blackboard-inscribed-with-scientific-formulas-and-calculations-in-physics-and-mathematics-can.jpg?s=612x612&w=0&k=20&c=sRLsJbHUVOYvZ_M16hti4fF9NM0RysfjAPUQrCJ8o4U=',
+                            'https://www.ntu.edu.sg/images/librariesprovider124/default-album/general-images/sss-webrevamp/sss_econs_coinsb6efd5c0-2a2a-4c0e-aa83-669f44598aea.jpg?Status=Master&sfvrsn=ee6510c_3',
+                            'https://www.udc.edu/social-udc/wp-content/uploads/sites/24/2018/03/Importance-of-Psychology_UDC.jpg',
+                            'https://heritageinstitute.in/wp-content/uploads/2019/01/Advanced-English.png',
+                            'https://dualcreditathome.com/wp-content/uploads/2014/02/history.jpg'],
+                'Course_schoolyear': [2023, 2023, 2024, 2024, 2024, 2023, 2023, 2024, 2023],  # Example school years
+                'Timetable': ['10.30-12.30', '13.00-15.00', '09.00-11.00', '10.30-12.30',
+                            '13.00-15.00', '09.00-11.00', '10.30-12.30', '13.00-15.00', '09.00-11.00']  # Example Timetable
+            }
+            # Create a DataFrame from the course data
+            df_courses = pd.DataFrame(course_data)
+            # Define header background image and text
+            header_bg_image = 'https://cdn.discordapp.com/attachments/1081112018360221696/1237724333326204928/asasdqweq.JPG?ex=663cb01c&is=663b5e9c&hm=950f617aa089f3d67c5e26c60c3e92c6dccd389fe9c86bbfefbd907a2dd3d4cf&'
+            header_text = """ 
+            <h1 style="color: white; text-align: center; margin-left: 40px; margin-top: 40px;">Helping You Grow</h1>
+            <br>
+            <h5 style="color: white; font-weight: 100; text-align: center; margin-left: 40px; margin-top: 10px;">Learning is the compass that guides us through the uncharted territories of knowledge</h5>
+            <h5 style="color: white; font-weight: 100; text-align: center; margin-left: 40px; margin-top: 10px;">illuminating our path with the brilliance of understanding.</h5>
+            """
+
+
+            # Page title
+            # Display header with background image and text
+            st.markdown(
+                f"""
+                <div style="padding: 0; background-size: cover;">
+                    <div style="background-image: url('{header_bg_image}'); filter: blur(2px); position: absolute; top: 0; left: 0; width: 100%; height: 350px; background-size: contain;"></div>
+                    <div style="position: relative; z-index: 0; margin-top: 50px; margin-bottom: 150px;">{header_text}</div>
+                </div>""",unsafe_allow_html=True
+            )
+
+            with st.container(border=True,height=1000):
+                st.markdown(f"""<h2 style=" text-align: center; margin-bottom:30px;">Active courses now</h2>""", unsafe_allow_html=True)
+                col1, col2, col3= st.columns(3)
+                
+                for index, course in df_courses.iterrows():
+                    with eval(f"col{index % 3+ 1}"):  # Alternate between the four columns
+                        col4, col5, col6= st.columns(3)
+                        with col5:
+                            st.markdown(
+                                f"""<div style="height: 133px; width: 200px; overflow: hidden; border-radius: 15px; margin-top:20px;">
+                                    <img src="{course['Image URL']}" alt="{course['Course Name']}" style="width: 100%; height: 100%; object-fit: cover;">
+                                </div>""",
+                                unsafe_allow_html=True
+                            )
+                        st.markdown(f"""<h4 style=" text-align: center; margin-bottom:10px;">{course['Course Name']}</h4>""", unsafe_allow_html=True)
+                        with st.container(border=True,):
+                            st.write(f"**Course ID:** {course['Course ID']}")
+                            st.write(f"**Instructor:** {course['Instructor']} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; **Instructor ID:** {course['Instructor_ID']}")
+                            st.write(f"**Description:** {course['Description']}")
+                            st.write(f"**Course School Year:** {course['Course_schoolyear']}")
+                            st.write(f"**Timetable:** {course['Timetable']}")
+                            st.button(f"**Enrollment** {course['Course ID']}")
+
         # student_df = invoice_df.drop(columns=['password'])
         # ui.table(student_df)
 
